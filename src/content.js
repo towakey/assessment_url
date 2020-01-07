52c44bc7f72642ba6c236688ee55a8c89bfb4501
towakey <kyosgame@gmail.com>
2020-01-08 01:40:32 +0900

    検索結果エリアの背景色を変更してみた

--- src/content.js
+++ src/content.js
@@ -1,1 +1,5 @@
-window.alert('Google Search');
+// window.alert('Google Search');
+var search_g = document.getElementsByClassName('g');
+for(i=0;i<search_g.length;i++){
+  search_g[i].style.backgroundColor = 'red';
+}
